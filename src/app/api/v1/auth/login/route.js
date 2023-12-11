0139283554b64e1bd28e54564a6a44a76a546851
AzhamDev <dev.azham@gmail.com>
2023-12-11 22:09:43 +0700

    bcrypt updated

--- src/app/api/v1/auth/login/route.js
+++ src/app/api/v1/auth/login/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/utils/prisma'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
 
 export async function POST(req) {
   const { email, password } = await req.json()
@@ -17,29 +18,33 @@ export async function POST(req) {
       return NextResponse.json({ message: "invalid email" }, { status: 204 })
     }
 
-    const payload = {
-      id: findUser.id,
-      email: findUser.email,
-      name: findUser.name
+    // const payload = {
+    //   id: findUser.id,
+    //   email: findUser.email,
+    //   name: findUser.name
+    // }
+
+    // const token = jwt.sign(payload, process.env.SECRET_KEY)
+
+    const isPasswordMatch = await bcrypt.compare(password, findUser.password)
+
+    if (!isPasswordMatch) {
+      return NextResponse.json({ message: "Invalid Password" }, { status: 203 })
     }
 
-    const token = jwt.sign(payload, process.env.SECRET_KEY)
-    console.log(token)
+    // if (findUser.password === password) {
+    //   const res = NextResponse.json({ message: "logged in successfully" }, { status: 200 })
+    //   res.cookies.set("token", token)
 
-    if (findUser.password === password) {
-      const res = NextResponse.json({ message: "logged in successfully" }, { status: 200 })
-      res.cookies.set("token", token)
+    //   return res;
+    // }
 
-      return res;
-    }
+    return NextResponse.json({ message: "Login Succesfully" }, { status: 200 })
 
 
-    return NextResponse.json({ message: "invalid password" }, { status: 204 });
 
   } catch (error) {
     console.log(error)
     return NextResponse.json({ error: "Error logging in" }, { status: 500 })
   }
-
-
 }
--- src/app/api/v1/auth/register/route.js
+++ src/app/api/v1/auth/register/route.js
@@ -1,16 +1,18 @@
 import { NextResponse } from 'next/server'
 import { prisma } from '@/utils/prisma'
+import bcrypt from 'bcrypt'
 
 export async function POST(req) {
   const { name, email, password } = await req.json()
 
   // create user to database
   try {
+    const hashedPassword = await bcrypt.hash(password, 10)
     const createUser = await prisma.users.create({
       data: {
         name,
         email,
-        password
+        password: hashedPassword
       }
     });
     return NextResponse.json({ message: "Creating User Successfully", data: createUser }, { status: 201 })
